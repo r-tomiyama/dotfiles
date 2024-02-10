0067e7a1e792295ece7fc9f678d17a3d16ca24f6
r-tomiyama <peko858.ab@gmail.com>
2024-02-10 11:59:26 +0900

    wip

---
+++ clone-github-repo.sh
@@ -0,0 +1,7 @@
+git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-completions ~/.oh-my-zsh/custom/plugins/zsh-completions
+
+# https://github.com/ohmyzsh/ohmyzsh
