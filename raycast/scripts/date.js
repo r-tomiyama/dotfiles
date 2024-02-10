8f3267004f4054d24b1eaaf3ab2e917d4a59f0af
r-tomiyama <peko858.ab@gmail.com>
2024-02-10 23:30:30 +0900

    update

---
+++ raycast/scripts/date.js
@@ -0,0 +1,26 @@
+#!/usr/bin/env node
+
+// Required parameters:
+// @raycast.schemaVersion 1
+// @raycast.title date
+// @raycast.mode compact
+
+// Optional parameters:
+// @raycast.icon 🤖
+// @raycast.packageName date
+
+// Documentation:
+// @raycast.author r-tomiyama
+// @raycast.authorURL https://raycast.com/r-tomiyama
+
+const currentDate = new Date();
+const year = currentDate.getFullYear();
+const month = String(currentDate.getMonth() + 1).padStart(2, '0');
+const day = String(currentDate.getDate()).padStart(2, '0');
+const hour = String(currentDate.getHours()).padStart(2, '0');
+const minute = String(currentDate.getMinutes()).padStart(2, '0');
+
+const formattedDateTime = `${year}${month}${day}${hour}${minute}`;
+formattedDateTime;
+
+console.log(formattedDateTime);
